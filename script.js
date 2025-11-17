71a6213e3dffa0465b136986497ac1eef4e03a83
merakleee <mahammedabdallahslimane@gmail.com>
2025-11-18 00:17:52 +0100

    Adding some changes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function calculateAverage() {
-    // Read input values
     let archi_td = parseFloat(document.getElementsByName('archi_td')[0].value) || 0;
     let archi_exam = parseFloat(document.getElementsByName('archi_exam')[0].value) || 0;
     let algo_td = parseFloat(document.getElementsByName('algo_td')[0].value) || 0;
@@ -16,7 +15,6 @@ function calculateAverage() {
     let ang_td = parseFloat(document.getElementsByName('ang_td')[0].value) || 0;
     let ang_exam = parseFloat(document.getElementsByName('ang_exam')[0].value) || 0;
 
-    // Subject averages
     let archi_avg = archi_td * 0.4 + archi_exam * 0.6;
     let algo_avg = algo_td * 0.2 + algo_tp * 0.2 + algo_exam * 0.6;
     let sys_avg = sys_td * 0.4 + sys_exam * 0.6;
@@ -25,7 +23,6 @@ function calculateAverage() {
     let log_avg = log_td * 0.4 + log_exam * 0.6;
     let ang_avg = ang_td * 0.4 + ang_exam * 0.6;
 
-    // Coefficients
     let archi_coef = 3;
     let algo_coef = 3;
     let sys_coef = 3;
@@ -34,7 +31,6 @@ function calculateAverage() {
     let log_coef = 3;
     let ang_coef = 2;
 
-    // Weighted sum and semester average
     let weighted_sum =
         archi_avg * archi_coef +
         algo_avg * algo_coef +
@@ -44,28 +40,41 @@ function calculateAverage() {
         log_avg * log_coef +
         ang_avg * ang_coef;
 
-    let semester_average = (weighted_sum / 20).toFixed(2);
+    let semester_average = parseFloat((weighted_sum / 20).toFixed(2));
 
-    document.getElementById('averageResult').innerText = "Semester Average: " + semester_average;
-
-    let comment = "";
-    let emoji = "";
-    if (semester_average < 5) {
-        comment = "yeah you're cooked!";
-        emoji = "🥀"; // wilted flower
-    } else if (semester_average < 10) {
-        comment = "zeyer rohak hbibi!";
-        emoji = "😔"; // pensive face
-    } else if (semester_average < 14) {
-        comment = "gg you did well!";
-        emoji = "👏"; // clap
-    } else {
-        comment = "KHABACH!";
-        emoji = "☝️🤓"; // point up and nerd face
+    if (semester_average < 0 || semester_average > 20) {
+        console.log("ERROR");
+        document.getElementById("averageResult").innerHTML = "ERROR: Average must be between 0 and 20";
+        document.getElementById("averageResult").style.color = "red";
+        document.getElementById("averageResult").classList.remove("result-bad", "result-mid", "result-good", "result-excellent");
+        return;
     }
 
-    document.getElementById('averageResult').innerHTML =
-        "Semester Average: " + semester_average + "<br><span>" + comment + " " + emoji + "</span>";
+    let comment = "", emoji = "", resultClass = "";
+
+    if (semester_average < 5) {
+        comment = "yeah you're cooked!";
+        emoji = "🥀";
+        resultClass = "result-bad";
+    } else if (semester_average < 10) {
+        comment = "zeyer rohak hbibi!";
+        emoji = "😔";
+        resultClass = "result-mid";
+    } else if (semester_average < 14) {
+        comment = "gg you did well!";
+        emoji = "👏";
+        resultClass = "result-good";
+    } else if (semester_average <= 20) {
+        comment = "KHABACH!";
+        emoji = "☝️🤓";
+        resultClass = "result-excellent";
+    }
+
+    const resultDiv = document.getElementById('averageResult');
+    resultDiv.classList.remove("result-bad", "result-mid", "result-good", "result-excellent", "result-animate");
+    resultDiv.classList.add(resultClass, "result-animate");
+
+    resultDiv.innerHTML = "Semester Average: " + semester_average + "<br><span>" + comment + " " + emoji + "</span>";
+    resultDiv.style.color = "";
+    setTimeout(() => resultDiv.classList.remove("result-animate"), 450);
 }
-
-
